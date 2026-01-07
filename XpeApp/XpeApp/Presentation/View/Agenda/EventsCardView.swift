86eeaab5654c77d81023501789395a08a228e780
Théo Lebègue <theo.lebegue@icloud.com>
2026-01-07 16:45:24 +0100

    feat(events): update tag display to show date range if endDate exists

--- XpeApp/XpeApp/Presentation/View/Agenda/EventsCardView.swift
+++ XpeApp/XpeApp/Presentation/View/Agenda/EventsCardView.swift
@@ -37,12 +37,15 @@ struct EventCard: View {
 
         var result: [TagPill] = []
 
-        // Add the event date
-        result.append(TagPill(label: dateDayAndMonthFormatter.string(from: event.date), backgroundColor: tagColor))
-        
-        // Add the event endDate
+        // Add the event date range as a single tag if endDate exists
         if let endDate = event.endDate {
-            result.append(TagPill(label: dateDayAndMonthFormatter.string(from: endDate), backgroundColor: tagColor))
+            let startStr = dateDayAndMonthFormatter.string(from: event.date)
+            let endStr = dateDayAndMonthFormatter.string(from: endDate)
+            let rangeLabel = "Du: \(startStr) Au: \(endStr)"
+            result.append(TagPill(label: rangeLabel, backgroundColor: tagColor))
+        } else {
+            // Only start date
+            result.append(TagPill(label: dateDayAndMonthFormatter.string(from: event.date), backgroundColor: tagColor))
         }
         // Format the time strings by removing the last three characters ("12:00:00" -> "12:00")
         func formatTime(_ time: String) -> String {
